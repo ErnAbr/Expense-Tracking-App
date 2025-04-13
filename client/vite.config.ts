3d5bd36a2a08f45ebc8c0c719855b91ff54f70f7
ErnAbr <ern_abr@yahoo.com>
2025-04-13 20:45:48 +0300

    setting up react testing library and vitest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,4 +7,9 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./jest.setup.ts",
+  },
+});
